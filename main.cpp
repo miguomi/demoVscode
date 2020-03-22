de7d47bd11cab2f9b97e6d7630e521b66d60c4d4
miguomi <guoguo2018666@gmail.com>
2020-03-22 11:16:35 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int i = 1;
+    std::cout << "hello vscode" << std::endl;
+    while(true){
+        i++;
+        std::cout << "hello vscode" << std::endl;
+    }
+    return 0;
+}
+
